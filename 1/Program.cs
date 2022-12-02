5e3a968732572b35959f8d6fe1b21e898fc4e8fc
tkachevdg <tkachevdg@gmail.com>
2022-12-02 16:42:51 +0600

    максимальное число из двух чисел

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,11 +1,11 @@
 ﻿int a;
 int b;
-int c;
+//int c;
 int max;
 
 string UA;
 string UB;
-string UC;
+//string UC;
 
 
 
@@ -13,19 +13,21 @@ Console.Write($"Введите первое число: ");
 UA = Console.ReadLine()!;
 Console.Write($"Введите второе число: ");
 UB = Console.ReadLine()!;
-Console.Write($"Введите третье число: ");
-UC = Console.ReadLine()!;
+//Console.Write($"Введите третье число: ");
+//UC = Console.ReadLine()!;
 
 a = int.Parse(UA);
 b = int.Parse(UB);
-c = int.Parse(UC);
+//c = int.Parse(UC);
 
 if(a > b)
 { max = a; }
 else
 { max = b; }
 
-if(max>c)
-{Console.Write($"Максимальное число = {max}\n");}
-else
-{{Console.Write($"Максимальное число = {c}\n");}}
+//if(max>c)
+//{Console.Write($"Максимальное число = {max}\n");}
+//else
+//{{Console.Write($"Максимальное число = {c}\n");}}
+
+Console.Write($"Максимальное число = {max}\n");
