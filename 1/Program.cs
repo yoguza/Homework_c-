8f3d5aa4bc61c68bdc362da6aacc0b4921ea2f99
tkachevdg <tkachevdg@gmail.com>
2022-12-03 14:56:39 +0600

    четное-нечетное

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,33 +1,13 @@
-﻿int a;
-int b;
-int c;
-int max;
-
+﻿//int a;
 string UA;
-string UB;
-string UC;
-
-
-
-Console.Write($"Введите первое число: ");
-UA = Console.ReadLine()!;
-Console.Write($"Введите второе число: ");
-UB = Console.ReadLine()!;
-Console.Write($"Введите третье число: ");
-UC = Console.ReadLine()!;
-
-a = int.Parse(UA);
-b = int.Parse(UB);
-c = int.Parse(UC);
-
-if(a > b)
-{ max = a; }
-else
-{ max = b; }
-
-if(max>c)
-{Console.Write($"Максимальное число = {max}\n");}
-else
-{{Console.Write($"Максимальное число = {c}\n");}}
-
-Console.Write($"Максимальное число = {max}\n");
+//Console.Write("Введите первое число: ");
+//UA = Console.ReadLine()!;
+//a = int.Parse(UA);
+//if (a % 2 == 1)
+//{
+//Console.WriteLine("число нечетное")!;
+//}
+//else
+//{
+//Console.WriteLine("Число четное")!;
+//}
